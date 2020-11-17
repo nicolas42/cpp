14a6b0a11bdf5e6ee093067cb224b0097f0d3909
Nick2 <Nick2@Morty.local>
2020-11-17 15:13:12 +1000

    lol comments

--- solitary_header_files/main.cpp
+++ solitary_header_files/main.cpp
@@ -18,23 +18,25 @@ int main(){
 
     const double t = 2*M_PI;
 
+    mv::type x = mv::vec(1,0,0);
+
     mv::mul(2, mv::vec(1,1,0), mv::vec(1,1,0));
     mv::mul(2, mv::vec_s(1, t/8, t/4), mv::vec_s(1, t/8, t/4));
-    mv::mul(2, mv::vec(1,0,0), mv::vec(0,1,0));
+    mv::mul(2, x, mv::vec(0,1,0));
     mv::mul(2, mv::vec_s(1, 0, t/8), mv::vec_s(1, 0, t/4));
-    mv::mul(3, mv::vec(1,0,0), mv::vec(0,1,0), mv::vec(0,0,1));
-    mv::mul(3, mv::vec(1,0,0), mv::vec(1,0,0), mv::vec_s(1,0,t/8));
+    mv::mul(3, x, mv::vec(0,1,0), mv::vec(0,0,1));
+    mv::mul(3, x, x, mv::vec_s(1,0,t/8));
 
     mv::type v, a, b, spinor1, spinor2;
 
-    v = mv::vec(1,0,0);
+    v = x;
     a = mv::vec_s(1, t/8, t/4);
     b = mv::vec_s(1, t/8, t/8);
     mv::mul(5, b,a,v,a,b);
 
-    spinor1 = mv::mul(2, mv::vec(1,0,0), mv::vec_s(1,t/8,t/4));
-    spinor2 = mv::mul(2, mv::vec(1,0,0), mv::vec_s(1,t/8,t/4));
-    mv::mul(3, mv::vec(1,0,0), spinor1, spinor2);
+    spinor1 = mv::mul(2, x, mv::vec_s(1,t/8,t/4));
+    spinor2 = mv::mul(2, x, mv::vec_s(1,t/8,t/4));
+    mv::mul(3, x, spinor1, spinor2);
 
     mv::mul(2, mv::trivector(3), mv::trivector(4));
     
